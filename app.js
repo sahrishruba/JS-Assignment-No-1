ed94424815ca1b63d251000015c405c1880c6c1a
sahrish ruba <sahrishrubabutt0@gmail.com>
2020-06-14 20:00:02 +0500

    practice java

---
+++ app.js
@@ -0,0 +1,100 @@
+// ****Ch#1 Task #1 **** //
+
+// alert("Hello Visitors");
+
+
+// ****Ch#1 Task #2 **** 
+
+// alert("Error! Please Enter a valid password");
+
+
+// ****Ch#1 Task #3 **** 
+
+// alert("Welcom to JS Land...\n Happy Coding!");
+
+
+// ****Ch#1 Task #4 **** 
+
+// alert ("Welcom to JS Land...");
+// alert(" Happy Coding! \n Prevent this page from creating additional dialogs ");
+
+
+// ****Ch#1 Task #5 **** 
+
+// alert("Hello... I can run JS through my web browser's cosole");
+
+// ****Ch#1 Task #6 **** 
+
+// on html page
+
+// ****Ch#1 Task #7 **** 
+
+// on html page
+
+
+// ****Ch#2 Task #1 **** 
+
+// var username = (" ");
+
+
+// ****Ch#2 Task #2 **** 
+
+// var myname = ("Sahrish Ruba");
+
+// ****Ch#2 Task #3 **** 
+
+// var message = ("Hello World");
+//  alert(message);
+
+// ****Ch#2 Task #4 **** 
+
+// var name = ("Jhone Doe");
+// var age = ( "15 years old");
+// var Qualification = (" certified Mobile Application Development");
+//  alert(name);
+//  alert(age);
+//  alert( Qualification);
+
+
+// ****Ch#2 Task #5 **** 
+
+// var a = (" PIZZA\n PIZZ\n PIZ\n PI\n P");
+// alert(a);
+
+
+// ****Ch#2 Task #6 **** 
+
+// var a = ("My Email Address is ");
+// var b =("abc@gmail.com");
+// var email = ( a+ b);
+// alert(email);
+
+
+// ****Ch#2 Task #7 **** 
+
+// var a = ("I am trying to learn from the Book ");
+// var b =("A smarter way to learn JavaScript");
+// var book = ( a+ b);
+// alert(book);
+
+// ****Ch#2 Task #8 **** 
+
+// var content =("Yah! I can write HTML content through Javascript");
+// document.write(content);
+
+
+// ****Ch#2 Task #9 **** 
+
+// var a = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// alert(a);
+
+
+// ****Ch#3 Task #1 **** 
+
+// var age = ("My age is 15 years old");
+// alert(age);
+
+// ****Ch#3 Task #2 **** 
+
+// ****Ch#3 Task #3 **** 
+// ****Ch#3 Task #4 **** 
